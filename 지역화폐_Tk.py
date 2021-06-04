feea0c804c1997da5f56bbcb4e69af2ec970ca42
yeonuyeonu <80469013+yeonuyeonu@users.noreply.github.com>
2021-06-05 01:49:42 +0900

    20210605(readme)
    주석 달아 놨습니다.
    
    한 번씩 읽어주세요.

--- 지역화폐_Tk.py
+++ 지역화폐_Tk.py
@@ -1,14 +1,15 @@
 from tkinter import *
 from tkinter import ttk, messagebox
-import tkinter.messagebox
 import folium
 import webbrowser
 from cefpython3 import cefpython as cef
 import sys
-import mimetypes
+# import mimetypes
 import smtplib
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
+
+import telepot
 
 #깃 데스크톱 채크용
 import urllib
@@ -19,12 +20,19 @@ import threading
 
 DataList = [] #검색한 가맹점 리스트
 is_getInfo = False #검색한 여부
-save_List = []
+save_List = [] #저장한 가맹점 리스트
+tele_List = [] #텔레그램 챗봇용 리스트
+
+# 챗 아이디
+chatId = '1841278408'
+# 텔레그램 챗 봇
+chatBot = telepot.Bot('1887297937:AAGhjXGGZyNY1yUPwRajFiyWj0Y3N0PY_UQ')
+#챗봇 이름 : 지역화폐 가맹점 텔레그램 봇 / @local_currency_telegram_bot
 
 class MainGUI:
-    
 
-    def InputSearchTab(self):#검색탭을 배치할 검색프레임 생성
+    # 검색탭을 배치할 검색프레임 생성
+    def InputSearchTab(self):
 
         self.frame_SearchTab = ttk.Frame(self.window, width=845, height=400, relief=RIDGE) 
         self.image_search = PhotoImage(file = "image/search.png")
@@ -42,9 +50,9 @@ class MainGUI:
         self.SearchButton = Button(self.frame_SearchTab, text= "검색!", command=self.SearchButtonAtion,relief="ridge", font= ("한수원 한돋움",13,'bold'),
                                     fg ="#ffffff",bg = '#005CB2',activebackground='#ffffff',activeforeground="#005CB2" )
         self.SearchButton.place(x = 390, y = 8)
-   
-    
-    def InputSaveTab(self):#저장탭을 배치할 검색프레임 생성
+
+    # 저장탭을 배치할 검색프레임 생성
+    def InputSaveTab(self):
 
         self.frame_SaveTab = ttk.Frame(self.window, width=845, height=380, relief=RIDGE)
         self.image_save = PhotoImage(file = "image/save.png")
@@ -58,7 +66,8 @@ class MainGUI:
         self.telegramButton = Button(self.frame_SaveTab,image=self.image_telegram, command=self.sendTelegram, bg = '#005CB2')
         self.telegramButton.place(x = 760, y = 200)
 
-    def InputLogo(self):#로고그림추가
+    # 로고그림추가
+    def InputLogo(self):
         
         self.logo = PhotoImage(file="image/logo.png")
         self.logo_label=Label(self.window, image=self.logo,bd=0)
@@ -68,7 +77,7 @@ class MainGUI:
         self.logo_label=Label(self.window, image=self.Ryon,bd=0)
         self.logo_label.place(x=480, y=0)
 
-     
+    #검색 버튼 입력 시 수행
     def SearchButtonAtion(self): #검색버튼누르면 돌아가는 함수 
         global DataList
         global is_getInfo
@@ -79,7 +88,8 @@ class MainGUI:
         
         #입력한 시/군을 가지고 xml받아와서 검색
         self.SearchFranchise() # 걍 가맹점이 영어로 Franchise였음
-    
+
+    #가맹점 검색 및 리스트박스에 출력
     def SearchFranchise(self):
         # -*- coding:cp949 -*-
         from xml.dom.minidom import parse, parseString
@@ -115,14 +125,12 @@ class MainGUI:
 
         else:
             print("OpenAPI request has been failed!! please retry")
-        
 
         self.ListBox.configure(state='normal')
 
         for i in range(len(DataList)):
             print(DataList[i])
             self.ListBox.insert(END,DataList[i][0])
-            # self.ListBox.insert(END, "\n")
 
         self.ListBox.pack()
         self.ListBox.place(x=5, y=43)
@@ -133,25 +141,13 @@ class MainGUI:
         self.ShowButton.place(x = 5, y = 370)
 
         #지도 프레임 추가
-        self.Mapframe = Frame(self.frame_SearchTab, width=340, height=250)
+        self.Mapframe = Frame(self.frame_SearchTab, width=340, height=250,bg = "#ffffff")
         self.Mapframe.place(x=450, y=130)
         url = 'file:///map.html'
         self.thread = threading.Thread(target=self.showMap, args=(self.Mapframe, url))
         self.thread.daemon = True
 
-
-        self.drawSahde()
-
-    def drawSahde(self):
-        if is_getInfo == False:  # is_getInfo = False
-            self.shadeLabel = Label(self.frame_SearchTab, width=350, height=250,bg="#ffffff")
-            self.shadeLabel.place(x=450, y=130)
-            print("흰")
-        else :
-            self.shadeLabel.place_forget()
-            print("없")
-
-
+    #받은 XMl정보를 리스트에 저장
     def ExtractFranchiseData(self, Doc):
         from xml.etree import ElementTree
 
@@ -180,8 +176,9 @@ class MainGUI:
                 
                 DataList.append(lst)
 
+    # cef 처음 한번 만들기
     def showMap(self, frame, url):
-        # cef 처음 한번 만들기
+
         sys.excepthook = cef.ExceptHook
         window_info = cef.WindowInfo(frame.winfo_id())
         window_info.SetAsChild(frame.winfo_id(), [0, 0, 340, 250])
@@ -192,18 +189,17 @@ class MainGUI:
         self.browser.LoadUrl(url)
         cef.MessageLoop()
 
+    # cef 갱신
     def LoadUrl(self):
-        # cef 갱신
         self.browser.LoadUrl('file:///map.html')
-    
+
+    #GET INFO
     def ShowInfo(self):
         global is_getInfo
         
-        #선택한 가게 정보 보기
-
+        #처음 정보받을 때는 bool 바꾸고, 그 이후로는 출력했던 가게 정보들 전부 삭제 (정보 출력 요소 초기화)
         if is_getInfo == False: #is_getInfo = False
             is_getInfo = True
-            self.drawSahde()
             self.thread.start()
         else :
             self.PrintselecLabel_INDUTYPE.place_forget()#업종별 사진 라벨 초기화
@@ -211,12 +207,11 @@ class MainGUI:
             self.selecLabel_LA.place_forget() #주소 라벨 초기화
             self.selecLabel_ZC.place_forget() #우편번호 라벨 초기화
 
-        self.is_on = False #해당 가맹점 북마크 on-true /off-false
+        #리스트 박스에서 선택중인 가게 이름
         self.selection = self.ListBox.get(self.ListBox.curselection())
-        # print(self.ListBox.index(self.ListBox.curselection()))#셀렉한가게인덱스
 
+        #업종명에 따라 출력 이미지 다르게 저장
         INDUTYPE = DataList[self.ListBox.index(self.ListBox.curselection())][5]
-
         if "보건" in INDUTYPE or "의원" in INDUTYPE or "약국" in INDUTYPE or "병원" in INDUTYPE:
             if "미용" in INDUTYPE:
                 self.INDUTYPE_image = PhotoImage(file="image/beauty.png")
@@ -237,31 +232,25 @@ class MainGUI:
         else :
             self.INDUTYPE_image = PhotoImage(file="image/ect.png")
 
-        # self.save = PhotoImage(file="image/save2.png")  # 가맹점 저장 버튼
+        #업종명에 맞는 이미지라벨 출력
         self.PrintselecLabel_INDUTYPE = Label(self.frame_SearchTab, image = self.INDUTYPE_image,bg ="#ffffff")
         self.PrintselecLabel_INDUTYPE.place(x = 460, y = 50)
 
+        # 가게이름, 주소 , 우편번호 출력
         self.selecLabel_NM = Label(self.frame_SearchTab, text=self.selection,font= ("한수원 한돋움",20),bg ="#ffffff",fg = '#005CB2')
         self.selecLabel_NM.place(x = 495, y = 50)
-
         if DataList[self.ListBox.index(self.ListBox.curselection())][1] == None:
             self.selecLabel_LA = Label(self.frame_SearchTab,text="주소 : - ",font=("한수원 한돋움", 9), bg="#ffffff", fg='#005CB2')
         else :
             self.selecLabel_LA = Label(self.frame_SearchTab, text=DataList[self.ListBox.index(self.ListBox.curselection())][1],font= ("한수원 한돋움",9,"underline"),bg ="#ffffff",fg = '#005CB2')
         self.selecLabel_LA.place(x = 460, y = 85)
-
         if DataList[self.ListBox.index(self.ListBox.curselection())][2] == None:
             self.selecLabel_ZC = Label(self.frame_SearchTab, text="우편번호: - ",font= ("한수원 한돋움",10),bg ="#ffffff",fg = '#005CB2')
         else:
             self.selecLabel_ZC = Label(self.frame_SearchTab, text="우편번호: "+DataList[self.ListBox.index(self.ListBox.curselection())][2],font= ("한수원 한돋움",10),bg ="#ffffff",fg = '#005CB2')
         self.selecLabel_ZC.place(x = 460, y = 105)
 
-        ####################################################################################################################################
-
-        self.seleclat = DataList[self.ListBox.index(self.ListBox.curselection())][3] #선택한 가맹점위도
-        self.seleclong = DataList[self.ListBox.index(self.ListBox.curselection())][4] #선택한 가맹점경도
-        # print(self.seleclong,self.seleclat)
-
+        # 길찾기 로드뷰 버튼 생성
         self.bb_image = PhotoImage(file="image/roadsign.png")
         self.myplace2selec = Button(self.frame_SearchTab, image = self.bb_image, command = lambda : self.Showkakao(2),bg ="#ffffff")
         self.myplace2selec.place(x = 795, y = 200)
@@ -269,19 +258,23 @@ class MainGUI:
         self.selecroadview = Button(self.frame_SearchTab, image = self.cc_image, command = lambda : self.Showkakao(3),bg ="#ffffff")
         self.selecroadview.place(x = 795, y = 260)
 
-        self.save = PhotoImage(file = "image/save3.png")#가맹점 저장 버튼
+        # 가맹점 저장 버튼 생성
+        self.save = PhotoImage(file = "image/save3.png")
         self.SaveFranchiseButton = Button(self.frame_SearchTab, image = self.save,bg = '#ffffff' ,borderwidth=0, relief="flat", command = self.SaveFranchise)
         self.SaveFranchiseButton.place(x = 720, y = 6)
 
+        # 위도 경도 받아서 지도 만들기
+        self.seleclat = DataList[self.ListBox.index(self.ListBox.curselection())][3] #선택한 가맹점위도
+        self.seleclong = DataList[self.ListBox.index(self.ListBox.curselection())][4] #선택한 가맹점경도
+        # print(self.seleclong,self.seleclat)
         m = folium.Map(location=[self.seleclat, self.seleclong], zoom_start=20)
-        # 마커 지정
         folium.Marker([self.seleclat, self.seleclong], popup=self.selection).add_to(m)
         # html 파일로 저장
         m.save('map.html')
-
         self.LoadUrl()
 
-    def SaveFranchise(self):#가맹점 저장
+    # 가맹점 저장 리스트 만들고 출력
+    def SaveFranchise(self):
         global save_List
         lst = []
 
@@ -320,6 +313,7 @@ class MainGUI:
 
         print(save_List)
 
+    #길찾기 로드뷰 브라우저 열기
     def Showkakao(self, num):
         if num == 2:
             url = "https://map.kakao.com/link/to/" + self.selection + "," + self.seleclat + "," + self.seleclong
@@ -328,18 +322,20 @@ class MainGUI:
             url = "https://map.kakao.com/link/roadview/" + self.seleclat + "," + self.seleclong
             webbrowser.open(url)
 
+    #버튼 눌렀을 때 받을 이멜 주소 받기
     def getEmail(self):
         self.email_top = Toplevel(self.window)
-        self.email_label = Label(self.email_top, text="받으실 메일 주소를 입력하세요")
+        self.email_label = Label(self.email_top, text="받을 메일 주소를 입력")
         self.email_label.place(x=10, y=10)
         self.email_entrybox = Entry(self.email_top, width=25)
         self.email_entrybox.place(x=10, y=35)
         self.email_top.geometry('200x90+200+300')
 
-        self.email_OK = Button(self.email_top, text='확인', command=self.googleLoginAndSendEmail)
+        self.email_OK = Button(self.email_top, text='확인!', command=self.googleLoginAndSendEmail)
         self.email_OK.place(x=80, y=60)
         self.email_top.deiconify()
 
+    #이멜 모내기
     def googleLoginAndSendEmail(self):
 
         htmlTxt = self.MakeHtmlDoc()
@@ -381,6 +377,7 @@ class MainGUI:
 
         mySmtp.close()
 
+    #구글에 보낼 정보 html로 만들기
     def MakeHtmlDoc(self): # HTML로 변환
         from xml.dom.minidom import getDOMImplementation
         # get Dom Implementation
@@ -423,10 +420,93 @@ class MainGUI:
 
         return newdoc.toxml()
 
-    def sendTelegram(self):
-        pass
+    # 텔레그램 명령어 판단
+    def handle(self, msg):
+        global chatId, chatBot, tele_List , save_List
+        #input = telepot.glance(msg)
+        text = msg['text']
+        args = text.split(' ')
 
-    def __init__(self): #TK 그리기
+        if text.startswith('검색') and len(args) > 1:
+            self.telegramlist(args[1])
+            for data in tele_List:
+                chatBot.sendMessage(chatId, '가맹점 : ' + data[0] + '\n주소 : ' + data[1] + '\n우편번호 : ' + str(int(data[2])) + '\n업종명 : ' + data[5])
+        elif text.startswith('저장리스트출력') :
+            for data in save_List:
+                chatBot.sendMessage(chatId, '가맹점 : ' + data[0] + '\n주소 : ' + data[1])
+        else :
+            chatBot.sendMessage(chatId, '모르는 명령어입니다.\n검색 [시군명] 또는 저장리스트출력 을 입력하세요.')
+
+    # 버튼 입력 시 저장리스트 텔레그램에서 출력
+    def sendTelegram(self):
+        global chatBot, save_List
+        for data in save_List:
+            chatBot.sendMessage(chatId, '가맹점 : ' + data[0] + '\n주소 : ' + data[1])
+
+    # 텔레그램 봇으로 메세지 보내기
+    def sendMessage(self, message):
+        global chatBot, chatId
+        chatBot.sendMessage(chatId, message)
+
+    # 텔레그램용 openapi 저장 리스트만들기
+    def telegramlist(self,text):
+        # -*- coding:cp949 -*-
+        from xml.dom.minidom import parse, parseString
+        from xml.etree import ElementTree
+        global tele_List
+
+        tele_List = []
+
+        headers = {
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/605.1.15',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+        }
+        # 오픈API 한글로 받는거
+        conn = http.client.HTTPSConnection("openapi.gg.go.kr")
+        hangul_utf8 = urllib.parse.quote(text)
+
+        conn.request("GET",
+                     "/RegionMnyFacltStus?KEY=a5ff90a0a64c48ee83f8ff3250b31afd&pIndex=3&pSize=10&SIGUN_NM=" + hangul_utf8,
+                     headers=headers)  # 가게이름
+        req = conn.getresponse()
+        # print(conn,hangul_utf8)
+        # print(req.status,req.reason)
+        Doc = (req.read().decode('utf-8'))
+
+        if req.status == 200:
+            if Doc == None:
+                print("에러")
+            else:
+                tree = ElementTree.fromstring(Doc)
+
+                # Acc 엘리먼트를 가져옵니다.
+                itemElements = list(tree.iter("row"))  # return list type
+                for item in itemElements:
+                    Franchise_name = item.find("CMPNM_NM")  # 상호명
+                    Franchise_LOTNO_add = item.find("REFINE_LOTNO_ADDR")  # 도로명주소
+                    Franchise_ZIP_CD = item.find("REFINE_ZIP_CD")  # 우편번호
+                    Franchise_WGS84_LAT = item.find("REFINE_WGS84_LAT")  # 위도
+                    Franchise_WGS84_LOGT = item.find("REFINE_WGS84_LOGT")  # 경도
+                    Franchise_INDUTYPE_NM = item.find("INDUTYPE_NM")  # 업종명
+
+                    if len(Franchise_name.text) > 0:
+                        lst = []
+                        lst.append(Franchise_name.text)
+                        lst.append(Franchise_LOTNO_add.text)
+                        lst.append(Franchise_ZIP_CD.text)
+                        lst.append(Franchise_WGS84_LAT.text)
+                        lst.append(Franchise_WGS84_LOGT.text)
+                        lst.append(Franchise_INDUTYPE_NM.text)
+
+                        tele_List.append(lst)
+
+    # TK 그리기
+    def __init__(self):
         
         self.window = Tk()
         self.window.title("스트레스는 지역화폐로 풀자!")
@@ -475,9 +555,10 @@ class MainGUI:
         self.InputSearchTab()
         self.InputSaveTab()
         self.InputLogo()
-        self.seleclat = 37.3402849 #위도
-        self.seleclong = 126.7313189 #경도
+        self.seleclat = None #위도
+        self.seleclong = None #경도
 
+        chatBot.message_loop(self.handle)
 
         self.window.mainloop()
 
